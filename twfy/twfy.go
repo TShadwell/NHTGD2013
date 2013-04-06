67ca445b98d3ce4fd41e75b77f9cb49e499379db
Thomas Shadwell <thomas.shadwell@googlemail.com>
2013-04-06 11:37:29 +0100

    It's a start

---
+++ twfy/twfy.go
@@ -0,0 +1,10 @@
+package twfy
+
+func (a *API) GetMPById(i PersonID)
+func (a *API) GetMpsForParty(p Party) {
+
+}
+func (a *API) GetMPById(i PersonID)
+func (a *API) GetMPById(i PersonID)
+func (a *API) GetMPById(i PersonID)
+
---
+++ twfy/types.go
@@ -0,0 +1,43 @@
+/*
+	Package twfy impliments basic Go bindings
+	for the TheyWorkForYou API.
+*/
+package twfy
+
+const (
+	Liberal_Democrat Party = "liberal democrat"
+	Conservative Party = "conservative"
+	Labour Party = "labour"
+)
+
+type (
+	//Represents one person- an MP usually
+	PersonID uint64
+	MemberID uint64
+	//Your TWFY API key
+	ApiKey string
+	Error string
+
+	API struct {
+		Key ApiKey
+	}
+
+	Party string
+	Constituency string
+
+	Person struct {
+		MemberID  MemberID `json:"member_id"`
+		PersonID PersonID `json:"person_id"`
+
+
+		Name string `json:"name"`
+		Party Party `json:"party"`
+		Constituency Constituency `json:"constituency"`
+	}
+
+)
+
+func (e Error) Error() string {
+	return string(e)
+}
+
