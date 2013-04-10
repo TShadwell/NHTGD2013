8bdd61b882180210f08b8b2ba0f85ff3768e307c
Thomas NJ Shadwell <thomas.shadwell@googlemail.com>
2013-04-10 21:52:00 +0100

    Doc completed

--- database/level/errors.go
+++ database/level/errors.go
@@ -4,12 +4,16 @@ type Error uint8
 
 const (
 	Already_Open Error = iota
+	Not_Opened
 )
 
-func (e Error) String() (o string){
+func (e Error) Error() (o string){
 	switch e{
 	case Already_Open:
 		o = "Database was already open."
+	case Not_Opened:
+		o = "Database has not yet been opened."
 	}
 	return
 }
+
